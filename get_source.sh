a315e4d8669487cb946416ef45d028d2c345efe7
ijjjjjjjjjjj <le2746668@gmail.com>
2026-05-14 20:17:24 +0700

    Create get_source.sh

---
+++ get_source.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+read -p "Nhập URL mục tiêu để lấy code: " target
+curl -s "$target" > savehack.txt
+echo "[✓] Đã tải toàn bộ code về savehack.txt"
+echo "[!] Bây giờ hãy dùng lệnh: nano savehack.txt để sửa theo ý bạn."
