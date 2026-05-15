fdbe61c4827d25db802e0433fb15bb07a468982d
ijjjjjjjjjjj <le2746668@gmail.com>
2026-05-15 10:42:04 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-# Tự động cài đặt môi trường
-echo "[*] Đang tối ưu hóa môi trường Termux..."
-pkg update -y && pkg install python -y
-pip install grequests requests colorama beautifulsoup4
-
-# Cấp quyền thực thi cho chính nó và file python
-chmod +x start.sh
-chmod +x smart_attack.py
-
 clear
 echo "==============================================="
 echo "   WEREWOLF ULTIMATE - SCAM DESTROYER v2.0"
 echo "        3 PHƯƠNG ÁN TẤN CÔNG TỔNG LỰC"
 echo "==============================================="
-read -p "Nhập link web lừa đảo (VD: https://abc.com): " target
+read -p "Nhập link mục tiêu: " target
+
+# Bước quan trọng: Di chuyển vào thư mục chứa code
+cd ~/Werewolf/Tools
+
+# Chạy tool với link mục tiêu đã nhập
 python smart_attack.py "$target"
